023889b1e3098bdf9efd2490924996873d888e59
Matias Lopez <imatlopez@gmail.com>
2017-09-30 23:30:09 -0400

    Get lvl1 component coverage to 100%

--- test/01c._html.js
+++ test/01c._html.js
@@ -21,6 +21,14 @@ describe('lib/utils/_html', function () {
     html: cheerio.load('<p class="class1 class3" ht-test="old">test</p>')('p')
   };
 
+  it('only needs a tag', function() {
+    const _obj = {
+      type: 'html',
+      tag: 'p'
+    };
+    expect( html(_obj) ).to.equal('<p></p>');
+  });
+
   it('creates object correctly', function () {
     const $ = cheerio.load( html(obj) )(obj.tag);
     
@@ -48,12 +56,42 @@ describe('lib/utils/_html', function () {
   });
 
   describe('throws', function () {
+    const _obj = {
+      type: 'html',
+      tag: 'p'
+    };
+
     it('when tag mismatches', function () {
       const _opts = {
         html: cheerio.load('<h1>test</h1>')('h1')
       };
       
       expect( html.bind(null, obj, _opts) ).to.throw();
+    });
+
+    it('when attribute is not an object', function () {
+      _obj.attr = 42;
+
+      expect( html.bind(null, _obj) ).to.throw();
+    });
+
+    it('when attribute is an array', function () {
+      _obj.attr = [];
+
+      expect( html.bind(null, _obj) ).to.throw();
+    });
+
+    it('when attribute is not an object', function () {
+      _obj.attr = 42;
+
+      expect( html.bind(null, _obj) ).to.throw();
+    });
+
+    it('when class is not an array', function () {
+      delete _obj.attr;
+      _obj.class = 42;
+
+      expect( html.bind(null, _obj) ).to.throw();
     });
   });
 });
